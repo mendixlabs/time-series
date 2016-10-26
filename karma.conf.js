aac2a845a43f355da6d2f5fec7ad14e9223091fe
Andries Smit <mr.andries.smit@gmail.com>
2016-10-26 12:18:08 +0200

    Add line coverage to Karma

--- karma.conf.js
+++ karma.conf.js
@@ -22,17 +22,25 @@ module.exports = function(config) {
         ],
         exclude: [],
         preprocessors: {
-            "tests/test-index.js": [ "webpack", "sourcemap" ]
+            "tests/test-index.js": [ "webpack", "sourcemap" ],
+            "**/*.ts": [ "karma-typescript" ]
         },
         webpack: webpackConfig,
         webpackServer: { noInfo: true },
-        reporters: [ "progress", "kjhtml" ],
+        reporters: [ "progress", "kjhtml", "karma-typescript" ],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
         browsers: [ "Chrome" ],
         singleRun: false,
-        concurrency: Infinity
+        concurrency: Infinity,
+        karmaTypescriptConfig: {
+            reports: {
+                "html": "./dist/coverage",
+                "text-summary": ""
+            }
+        },
+        // singleRun: true
     })
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var webpack = require("webpack");
+var path = require("path");
 var CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
@@ -18,7 +19,13 @@ module.exports = {
         loaders: [
             { test: [/\.tsx?$/, /\.ts?$/], loaders: [ "ts-loader" ] },
             { test: /\.json$/, loader: "json" }
-        ]
+        ],
+        postLoaders: [ {
+            test: /\.ts$/,
+            loader: "istanbul-instrumenter",
+            include: path.resolve(__dirname, "src"),
+            exclude: /\.(spec)\.ts$/
+        } ],
     },
     devtool: "source-map",
     externals: [ "mxui/widget/_WidgetBase", "mendix/lang", "dojo/_base/declare" ],
