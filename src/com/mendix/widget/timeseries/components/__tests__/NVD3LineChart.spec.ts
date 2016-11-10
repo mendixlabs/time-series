97c6381d8cf89ed36ec125569c169e495875f424
Derrick Kwagala <derrick.kwagala@gmail.com>
2016-11-10 12:11:29 +0300

    Tests for default chart properties

--- src/com/mendix/widget/timeseries/components/__tests__/NVD3LineChart.spec.ts
+++ src/com/mendix/widget/timeseries/components/__tests__/NVD3LineChart.spec.ts
@@ -20,84 +20,90 @@ describe("NVD3LineChart", () => {
         width: 50
     };
 
-    it("should add a line graph", () => {
-        spyOn(nv.models, "lineChart");
+    it("should add a graph", () => {
+        spyOn(nv, "addGraph");
 
-        renderFull(chartProps);
-        setTimeout((done) => {
-            expect(nv.models.lineChart).toHaveBeenCalled();
-            done();
-        }, 0);
+        const chart = renderChart(chartProps).instance() as NVD3LineChart;
+        chart.componentDidMount();
+
+        expect(nv.addGraph).toHaveBeenCalled();
     });
 
-
-    it("should add a chart with smart default properties", () => {
-        let chart: nv.LineChart;
-        spyOn(nv.models, "lineChart").and.callFake(() => chart = nv.models.lineChart());
-
-        renderFull(chartProps);
-        setTimeout((done) => {
-            expect(chart.useInteractiveGuideline()).toBe(true);
-            expect(chart.showLegend).toBe(true);
-            expect(chart.showXAxis).toBe(true);
-            expect(chart.showYAxis).toBe(true);
-            expect(chart.duration).toEqual(350);
-
+    describe("default chart properties", () => {
+        let chart2: nv.LineChart;
+        beforeAll((done) => {
+            spyOn(nv.models, "lineChart").and.callFake(() => chart2 = nv.models.lineChart());
+            renderFull(chartProps);
             done();
-        }, 0);
+        });
+
+        it("should have an interactive guideline", () => {
+            setTimeout(() => {
+                expect(chart2.useInteractiveGuideline()).toBe(true);
+            }, 100);
+        });
+
+        it("should have a legend", () => {
+            setTimeout(() => {
+                expect(chart2.showLegend()).toBe(true);
+            }, 100);
+        });
+
+        it("should show the x-axis", () => {
+            setTimeout(() => {
+                expect(chart2.showXAxis()).toBe(true);
+            }, 100);
+        });
+
+        it("should show the y-axis", () => {
+            setTimeout(() => {
+                expect(chart2.showYAxis()).toBe(true);
+            }, 100);
+        });
+
+        it("should have an animation duration", () => {
+            setTimeout(() => {
+                expect(chart2.showLegend()).toBe(350);
+            }, 100);
+        });
+
     });
 
-    it("should render expected structure", () => {
+    it("should render a structure correctly", () => {
         const chart = renderChart(chartProps);
-        expect(chart).toMatchStructure(
-            DOM.div({ className: "nv-chart" },
+
+        expect(chart).toBeElement(
+            DOM.div({ className: "nv-chart", style: { height: 20, width: 50 } },
                 DOM.svg()
             )
         );
     });
 
-    it("should render div with style height", () => {
+    it("should render a div with a style height", () => {
         const output = renderChart(chartProps);
 
         expect(output.first().prop("style").height).toBe(20);
     });
 
-    it("should render div with style width", () => {
+    it("should render a div with a style width", () => {
         const output = renderChart(chartProps);
 
         expect(output.first().prop("style").width).toBe(50);
     });
 
-    describe("without datum values", () => {
-        it("should render chart with no data", () => {
-            const noDatum = {
-                chartProps: {},
-                datum: [ {} ],
-                height: 50,
-                width: 20
-            };
-            const chart = renderChart(noDatum);
-            expect(chart).toMatchStructure(
-                DOM.div({ className: "nv-chart" },
-                    DOM.svg()
-                )
-            );
+    xdescribe("", () => {
+        it("should add resize listener", () => {
+            //
+        });
+
+        it("should update chart on resize", () => {
+            // spy on the update function if its been called.
+        });
+
+        it("should remove resize listener", () => {
+            // mock the nv.util.windowsresize function with {clear: function}
+            // execute unmount and confirm if clear has been fired.
         });
     });
-
-//     describe("", () => {
-//         it("should add resize listener", () => {
-//             //
-//         });
-
-//         it("should update chart on resize", () => {
-//             // spy on the update function if its been called.
-//         });
-
-//         it("should remove resize listener", () => {
-//             // mock the nv.util.windowsresize function with {clear: function}
-//             // execute unmount and confirm if clear has been fired.
-//         });
-//     });
 
 });
