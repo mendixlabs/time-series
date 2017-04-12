b4735e99dd5633bf6d68b7e998119a1b234df303
Derrick Kwagala <derrick.kwagala@gmail.com>
2017-04-12 17:04:28 +0300

    Remove preview image

--- webpack.config.js
+++ webpack.config.js
@@ -28,8 +28,7 @@ module.exports = {
     plugins: [
         new CopyWebpackPlugin([
             { from: "src/**/*.js" },
-            { from: "src/**/*.xml" },
-            { from: "assets/Preview.png", to: "src/Preview.png"}
+            { from: "src/**/*.xml" }
         ], {
             copyUnmodified: true
         }),
