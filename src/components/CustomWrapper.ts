da05f081799cfaf2ce71f6b941c2ece1d0ddd04d
Andries Smit <mr.andries.smit@gmail.com>
2017-06-08 16:58:51 +0200

    Remove custom size wrapper

--- src/components/CustomWrapper.ts
+++
@@ -1,8 +0,0 @@
-// import { Component, DOM } from "react";
-
-// export class CustomWrapper extends Component<{ style?: string; class?: string; }, {}> {
-
-//     render() {
-//         return DOM.div({ className: this.props.class, style: this.parseStyle() }, this.props.children);
-//     }
-// }
