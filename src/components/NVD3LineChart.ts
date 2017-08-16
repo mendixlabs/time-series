e67386a310feed46e03dd9367c699a2de8db7bc8
Derrick Kwagala <derrick.kwagala@gmail.com>
2017-08-16 17:41:50 +0300

    Remove styling wrapper

--- src/components/NVD3LineChart.ts
+++ src/components/NVD3LineChart.ts
@@ -1,6 +1,7 @@
 import { select, time } from "d3";
 import { LineChart, Nvd3ResizeHandler, addGraph, models, utils } from "nvd3";
 import { Component, createElement } from "react";
+import { findDOMNode } from "react-dom";
 
 import { Series } from "./TimeSeries";
 import { HeightUnit, WidthUnit } from "../TimeSeries";
@@ -57,16 +58,11 @@ class NVD3LineChart extends Component<Nvd3LineChartProps, {}> {
     componentDidMount() {
         // Add height and display styles to react wrapper
         // Avoided use of clientHeight because content-area varies depending on styling.
-        const reactWrapper = this.svg && this.svg.parentElement && this.svg.parentElement.parentElement
-            && this.svg.parentElement.parentElement.parentElement
-            && this.svg.parentElement.parentElement.parentElement && this.svg.parentElement.parentElement.parentElement;
+        const reactWrapper = findDOMNode(this).parentElement as HTMLElement;
+
         if (this.props.heightUnit === "percentageOfParent" && reactWrapper) {
             reactWrapper.style.height = "100%";
             reactWrapper.style.display = "flex";
-            // hack: height on ReactCustomWidgetWrapper
-            if (reactWrapper.parentElement) {
-                reactWrapper.parentElement.style.height = "100%";
-            }
         }
 
         addGraph(() => this.renderChart(), this.chartEvents);
